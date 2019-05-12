509c75fddd623f643e83a43a24d6180ee2b65a37
mforets <mforets@gmail.com>
2019-05-12 17:18:21 +1000

    add tests for AffineMap

--- src/LazySet.jl
+++ src/LazySet.jl
@@ -44,9 +44,10 @@ The subtypes of `LazySet` (including abstract interfaces):
 julia> using LazySets: subtypes
 
 julia> subtypes(LazySet, false)
-17-element Array{Any,1}:
+18-element Array{Any,1}:
  AbstractCentrallySymmetric
  AbstractPolyhedron
+ AffineMap
  CacheMinkowskiSum
  CartesianProduct
  CartesianProductArray
--- test/runtests.jl
+++ test/runtests.jl
@@ -104,6 +104,7 @@ if test_suite_basic
     @time @testset "LazySets.CartesianProduct" begin include("unit_CartesianProduct.jl") end
     @time @testset "LazySets.ResetMap" begin include("unit_ResetMap.jl") end
     @time @testset "LazySets.SymmetricIntervalHull" begin include("unit_SymmetricIntervalHull.jl") end
+    @time @testset "LazySets.AffineMap" begin include("unit_AffineMap.jl") end
 
     # ======================
     # Testing set interfaces
